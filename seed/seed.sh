d11bc484debcbd200b9dc73e012d0540e5821244
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-10-22 14:51:46 +0200

    Changed database initialization

---
+++ seed/seed.sh
@@ -0,0 +1,7 @@
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=characters -u $DB_USER -p $DB_PWD -v --file=characters.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=items -u $DB_USER -p $DB_PWD -v --file=items.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=backgrounds -u $DB_USER -p $DB_PWD -v --file=backgrounds.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=feats -u $DB_USER -p $DB_PWD -v --file=feats.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=players -u $DB_USER -p $DB_PWD -v --file=players.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=classes -u $DB_USER -p $DB_PWD -v --file=classes.json
+mongoimport --host=$MONGO_HOST:$MONGO_PORT --db=testDB --collection=spells -u $DB_USER -p $DB_PWD -v --file=spells.json
